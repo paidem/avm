b19fc4f96ea05bb6d19d0e5c237dd377ce6a11be
Pavel Paidem <pavel.paidem@gmail.com>
2025-08-25 16:03:01 +0300

    Fix header name

--- modules/session/check_user_header.py
+++ modules/session/check_user_header.py
@@ -7,7 +7,7 @@ def check_header_auth():
     forwarded_user_header_name = os.environ.get('USERNAME_HEADER',None)
     admin_users_str = os.environ.get('ADMIN_USERS',None)
     if forwarded_user_header_name and admin_users_str:
-        header_user = request.headers.get('X-Forwarded-Preferred-Username', None)
+        header_user = request.headers.get(forwarded_user_header_name, None)
         admin_users = [user.strip() for user in admin_users_str.split(',') if user.strip()]
         if (header_user in admin_users):
            return True
